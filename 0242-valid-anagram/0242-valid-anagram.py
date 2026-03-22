dc3f04e16dda6602c92e3f1fbbbc5ea2727b6112
Tanu Singh Attri <tanusattri06@gmail.com>
2026-03-22 19:09:11 +0530

    Time: 10 ms (78.12%), Space: 19.4 MB (88.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        m= len(s)
+        n= len(t)
+        if m!=n:
+            return False
+        countS, countT={}, {}
+        for i in range (m):
+            countS[s[i]]= countS.get(s[i],0)+1
+            countT[t[i]]= countT.get(t[i],0)+1
+        return countS==countT
