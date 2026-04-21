1c0576cf4a927d76ca76b8826cd0eb0a6cbc60bd
Tanu Singh Attri <tanusattri06@gmail.com>
2026-04-21 09:24:55 +0530

    Time: 123 ms (52.65%), Space: 37.4 MB (11.57%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,21 +1,21 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         n= len(nums)
-        max_sum=0
-        curr_sum=0
-        left=0
-        window_set= set()
-        for right in range(n):
-            while nums[right] in window_set:
-                window_set.remove(nums[left])
-                curr_sum-= nums[left]
-                left+=1
-            window_set.add(nums[right])
-            curr_sum+= nums[right]
-            if right-left+1>k:
-                window_set.remove(nums[left])
-                curr_sum-= nums[left]
-                left+=1
-            if right-left+1==k:
-                max_sum= max(max_sum, curr_sum)
+        i,j= 0,0
+        max_sum, curr_sum= 0,0
+        count= {}
+        while j<n:
+            curr_sum+= nums[j]
+            count[nums[j]]= count.get(nums[j],0)+1
+            if j-i+1<k:
+                j+=1
+            elif j-i+1==k:
+                if len(count)==k:
+                    max_sum= max(max_sum,curr_sum)
+                curr_sum-= nums[i]
+                count[nums[i]]-=1
+                if count[nums[i]]==0:
+                    del count[nums[i]]
+                i+=1
+                j+=1
         return max_sum
