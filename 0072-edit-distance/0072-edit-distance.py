04fc00a570191631f38ae28e82d60edba3054228
Tanu Singh Attri <tanusattri06@gmail.com>
2026-04-21 19:46:14 +0530

    Time: 55 ms (31.25%), Space: 23 MB (30.45%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -3,9 +3,9 @@ class Solution:
         m= len(word1)
         n= len(word2)
         dp= [[0 for _ in range(n+1)] for _ in range(m+1)]
-        for i in range(m+1): #Base Case
+        for i in range(m+1):
             dp[i][0]= i
-        for j in range(n+1): #Base Case 
+        for j in range(n+1):
             dp[0][j]= j
         for i in range(1,m+1):
             for j in range(1,n+1):
