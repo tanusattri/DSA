150a7a64ebfaaea092b64ac0dd4c2e2cc4530967
Tanu Singh Attri <tanusattri06@gmail.com>
2026-04-26 09:41:57 +0530

    Time: 60 ms (71.67%), Space: 19.3 MB (54.83%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        n= len(nums)
+        result= [0]*n
+        for i in range(n):
+            result[i]= nums[(i+nums[i])%n]
+        return result
+            
